afe9336277c74de9fd241edd9b056706def0b9ce
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2022-01-22 17:56:25 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
 
 /* mode checkbox */
-var checkbox = document.getElementById('colorsmode');
-var Body=document.body;
+// var checkbox = document.getElementById('colorsmode');
+// var Body=document.body;
 
-function switchMode( ){
-    console.log("checked");
-    // document.body.classList.toggle("lightmode");
-    Body.classList.toggle("lightmode");
-}
+// function switchMode( ){
+//     console.log("checked");
+//     // document.body.classList.toggle("lightmode");
+//     Body.classList.toggle("lightmode");
+// }
 
-checkbox.addEventListener("click", switchMode);
+// checkbox.addEventListener("click", switchMode);
+
+// ===========================================================
+// ===========================================================
+
+// var modeKnop = document.getElementById('mode');
+// var Body=document.body;
+
+// function switchMode(){
+//     console.log("clicked");
+//     Body.classList.toggle("darkmode");
+    
+//     if(modeKnop.innerHTML==="Dark"){
+//         console.log("Light");
+//         modeKnop.innerHTML = "Light";
+//         Body.classList.add("darkMode")
+//     } else {
+//         console.log("Dark");
+//         modeKnop.innerHTML = "Dark";
+//         Body.classList.remove("darkMode")
+//     }
+// }
+
+// modeKnop.addEventListener('click', switchMode);
+
 
 // ====================================================
 // ====================================================
@@ -18,23 +42,62 @@ var navigtieknop = document.querySelector("header nav button");
 var navigtielist = document.querySelector("header nav");
 
 function openNavmenu(){
-    console.log("click");
+    console.log("clicked");
     navigtielist.classList.toggle("toonNavmenu");
 }
 
 navigtieknop.addEventListener("click", openNavmenu);
 
 
-// Works nav 
-var worksKnop = document.querySelector("main>nav button");
+// works nav 
+var worksKnop = document.querySelector('worksKnop');
 var workslist = document.querySelector("main>nav");
 
-function openWorksmenu(){
-    console.log("click");
-    workslist.classList.toggle("toonWorksmenu");
+document.addEventListener('load', function (openWorkmenu) {
+    // alert("It's loaded!")
+    console.log("loaded");
+  })
+
+function openWorkmenu(){
+    console.log("clicked");
+    workslist.classList.toggle("toonworksmenu");
+
 }
 
-worksKnop.addEventListener("click", openWorksmenu);
+worksKnop.addEventListener('click', openWorkmenu);
+
+worksKnop.addEventListener
+console.log("load");
+
+
+
+// ===========================================================
+// ===========================================================
+// var colorKnop = document.getElementById("colorsmode");
+// var Body=document.body;
+
+// if(localStorage.getItem("colormode") !== null){
+//     var colormode = JSON.parse(localStorage.getItem("colormode"));
+
+//     colorKnop.checked = colormode;
+
+//     if(colormode){
+//         document.documentElement.classList.add("darkMode");
+//     }
+// }
+
+// function switchMode(){
+//     if(colorKnop.checked){
+//         document.body.classList.add("darkMode");
+//         localStorage.setItem("colormode", JSON.stringify(true));
+//     }
+//     else {
+//         document.body.classList.remove("darkMode");
+//         localStorage.setItem("colormode", JSON.stringify(false));
+//     }
+// }
+
+// colorKnop.addEventListener('change', switchMode);
 
 
 
